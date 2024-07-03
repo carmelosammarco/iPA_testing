e094b33f1c185c3d0c05439f2dab831b2e2f0841
Carmelo <sammarcocarmelo@gmail.com>
2024-07-03 16:48:05 +0200

    TrackRecStudio IPA Generation

--- android/app/src/main/kotlin/com/example/the3dbay/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.the3dbay
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/main.dart
+++ lib/main.dart
@@ -1,83 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter_plus/webview_flutter_plus.dart';
+import 'package:flutter/services.dart';
+import 'package:trackrec_studio/logic/context/appContext.dart';
+import 'package:trackrec_studio/logic/db/db.dart';
+import 'package:trackrec_studio/routes/routeGenerator.dart';
 
-void main() {
-  runApp(const The3DBAY());
+void main() async{
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark.copyWith(
+    statusBarColor: Colors.white, // Color for Android
+    statusBarBrightness: Brightness.light //for IOS.
+  ));
+  await Db().init();
+  runApp(App());
 }
 
-class The3DBAY extends StatelessWidget {
-  const The3DBAY({super.key});
-
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: MainPage(),
-    );
-  }
-}
-
-class MainPage extends StatefulWidget {
-  const MainPage({super.key});
-
-  @override
-  State<MainPage> createState() => _MainPageState();
-}
-
-class _MainPageState extends State<MainPage> {
-  late WebViewControllerPlus _controler;
-
-  @override
-  void initState() {
-    _controler = WebViewControllerPlus()
-      ..loadFlutterAssetServer('assets/website/index.html')
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setBackgroundColor(const Color(0x00000000));
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 29, 49, 225),
-        title: const Text(''), 
-        leadingWidth: 100,
-        leading: IconButton(
-          onPressed: () => {
-          Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const MainPage()))
-        }, 
-            icon: const Icon(Icons.home, size: 40,),
-            color: Colors.white),
-
-        //leading: const Icon(Icons.menu),
-        //leading: Image.asset('assets/website/3dbay.jpg'),
-
-        //actions: [
-
-          //IconButton(
-            //onPressed: () => {}, 
-            //icon: const Icon(Icons.star),
-            //color: Colors.white),
-
-          //IconButton(
-            //onPressed: () => {}, 
-            //icon: const Icon(Icons.exit_to_app),
-            //color: Colors.white),
-       // ],
-
+    return MaterialApp(  
+      title: 'TrackRec Studio',
+      navigatorKey: navigatorState,
+      theme: ThemeData(
+        primaryColor: Colors.white, colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.red)
       ),
-
-      body: WebViewWidget(
-              controller: _controler,
-            ),
-          );
-  }
-
-  @override
-  void dispose() {
-    _controler.server.close();
-    super.dispose();
+      initialRoute: "/iniz",
+      onGenerateRoute: RouteGenerator.generateRoute
+    );
   }
 }
